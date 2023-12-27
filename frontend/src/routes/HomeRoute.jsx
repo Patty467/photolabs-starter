137e93605f4255b7332818ad9e5561fde2949596
Patrick Aucoin <aucoinp640@gmail.com>
2023-12-27 16:22:18 -0500

    uprate routes

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
-
+import React from "react";
+import TopNavigationBar from 'components/TopNavigationBar';
+import PhotoList from 'components/PhotoList';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 import '../styles/HomeRoute.scss';
 
-const HomeRoute = () => {
+const HomeRoute = (props) => {
+
+  const {
+    favorites,
+    isModalOpen,
+    selectedPhoto,
+    openModal,
+    closeModal,
+    toggleFavorite,
+    photoData,
+    topicData,
+    changeTopic
+  } = props
+
+  //check if there is a fave photo
+  const isFavePhotoExist = favorites.length > 0;
+
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigationBar 
+      isFavePhotoExist={isFavePhotoExist}
+      topicData={topicData}
+      changeTopic={changeTopic}
+      />
+      <PhotoList 
+      favorites={favorites} 
+      toggleFavorite={toggleFavorite} 
+      openModal={openModal} 
+      photoData={photoData}
+      />
+      {isModalOpen && 
+      <PhotoDetailsModal 
+        closeModal={closeModal} 
+        photo={selectedPhoto} 
+        favorites={favorites}
+        toggleFavorite={toggleFavorite}
+        photoData={photoData}
+      />}
     </div>
   );
 };
--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,14 +1,50 @@
+import PhotoFavButton from '../components/PhotoFavButton';
 import React from 'react';
-
+import PhotoList from '../components/PhotoList';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = (props) => {
+  const {
+    closeModal,
+    photo,
+    favorites,
+    toggleFavorite,
+    photoData
+  } = props
+
+  const isFavorited = favorites.includes(photo.id);
+  const similar_photos = photo.similar_photos;
+
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
-        <img src={closeSymbol} alt="close symbol" />
+      <button onClick={closeModal} className="photo-details-modal__close-button">
+      <img src={closeSymbol} alt="close symbol" />
       </button>
+
+      <div className="photo-modal__list">
+        <PhotoFavButton 
+          isFavorited={isFavorited} 
+          onClick={() => toggleFavorite(photo.id)}
+        />
+          <img className="photo-details-modal__image" src={photo.urls.full} />
+          <div className="photo-list-modal__user-details">
+            <img className="photo-list__user-profile" src={photo.user.profile}/>
+            <div className="photo-list__user-details-text">
+              <p className="photo-list__user-info"> {photo.user.username}</p>
+              <p className="photo-list__user-location"> {photo.location.city}, {photo.location.country}</p>
+          </div>
+      </div>
+      </div>
+      <div className='photo-modal__list'>
+        <h2>Similar Photos</h2>
+        <PhotoList 
+        favorites={favorites} 
+        toggleFavorite={toggleFavorite} 
+        photoData={similar_photos}
+        openModal={() => {}}
+      />
+      </div>
     </div>
   )
 };
